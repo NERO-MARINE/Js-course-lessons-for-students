380358b42321b1fea51dfb798a2ca732774e396a
King Nero <oghenerookerri@gmail.com>
2024-10-26 17:05:25 +0100

    added Higher order function assignments

--- Javascript/js/arrays/array.js
+++ Javascript/js/arrays/array.js
@@ -77,3 +77,75 @@ const booksWrittenByPreshAndJohn = books.filter(
 );
 
 console.log(booksWrittenByPreshAndJohn);
+
+//  Higher order functions Assignment
+
+const companies = [
+  {
+    name: "codebadger Technology",
+    yearFounded: 2020,
+    country: "Nigeria",
+    startUpCapital: 2000,
+  },
+  {
+    name: "Total Energies",
+    yearFounded: 2021,
+    country: "Canada",
+    startUpCapital: 5000,
+  },
+  { name: "NNPC", yearFounded: 1995, country: "USA", startUpCapital: 4000 },
+  {
+    name: "Bamako",
+    yearFounded: 1996,
+    country: "Nigeria",
+    startUpCapital: 1000,
+  },
+  {
+    name: "Zebra",
+    yearFounded: 2020,
+    country: "Nigeria",
+    startUpCapital: 7000,
+  },
+  {
+    name: "Amazon",
+    yearFounded: 1990,
+    country: "Canada",
+    startUpCapital: 1000,
+  },
+  { name: "Raycom", yearFounded: 2020, country: "USA", startUpCapital: 2500 },
+  {
+    name: "Spikes",
+    yearFounded: 1991,
+    country: "Nigeria",
+    startUpCapital: 3000,
+  },
+  { name: "Catly", yearFounded: 1991, country: "Canada", startUpCapital: 5000 },
+  {
+    name: "First Bank",
+    yearFounded: 2005,
+    country: "USA",
+    startUpCapital: 1000,
+  },
+  {
+    name: "Logcas",
+    yearFounded: 2004,
+    country: "Nigeria",
+    startUpCapital: 2000,
+  },
+  { name: "Focal", yearFounded: 2022, country: "Canada", startUpCapital: 5000 },
+  {
+    name: "klazon",
+    yearFounded: 2010,
+    country: "Canada",
+    startUpCapital: 1000,
+  },
+  { name: "Remow", yearFounded: 2008, country: "USA", startUpCapital: 2000 },
+];
+
+// 1) log only title of each company to your console
+// 2) log only the companies with a start up capital greater than 2000
+// 3) log only companies founded before year 2000
+// 4) log only companies founded after year 2000
+// 5) log only companies that are located in Nigeria and Canada
+// 6) log the total sum of start up captial by all companies
+// 7) log only companies founded in the year 2020
